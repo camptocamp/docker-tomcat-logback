f18f71b25ea518d34dffe44e1b48952b31b77e6c
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2016-06-22 15:04:36 +0200

    Specify the logback.xml location

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,4 +1,4 @@
-CLASSPATH=${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar:${CATALINA_HOME}/conf
+CLASSPATH=${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar
 
 for jar in ${CATALINA_HOME}/lib/* ${CATALINA_HOME}/extlib/*
 do
@@ -6,3 +6,5 @@ do
 done
 
 UMASK="0022"
+
+CATALINA_OPTS="${CATALINA_OPTS} -Dlogback.configurationFile=${CATALINA_HOME}/conf/logback.xml"
